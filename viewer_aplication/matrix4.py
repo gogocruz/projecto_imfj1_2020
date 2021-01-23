2f511edd61cc76f48ddc0426b0900ea60edf00da
DanielPinhao22007445 <74421663+DanielPinhao22007445@users.noreply.github.com>
2021-01-23 22:11:56 +0000

    Corrected not working with 3 "f"'s

---
+++ viewer_aplication/matrix4.py
@@ -0,0 +1,294 @@
+import vector4
+import numpy
+
+USE_NUMPY = False
+
+class InvalidMatrixOperationException(Exception):
+    """Exception thrown when there's an invalid operation with matrices"""
+    def __init__(self, op, type1, type2):
+        super().__init__(self)
+        self.op = op
+        self.type1 = type1
+        self.type2 = type2
+
+    def __str__(self):
+        """Returns a readable version of the exception"""
+        return f"Invalid matrix operation ({self.op}) between {self.type1} and {self.type2}!"
+
+
+class Matrix4:
+    def __init__(self, m=None):
+        if m is None:
+            self.m = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+        elif isinstance(m, (int, float)):
+            self.m = [[m, m, m, m], [m, m, m, m], [m, m, m, m], [m, m, m, m]]
+        else:
+            self.m = m
+
+    def __mul__(self, m):
+        if isinstance(m, Matrix4):
+            m2 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+            for i in range(0, 4):
+                for j in range(0, 4):
+                    m2[i][j] = 0
+                    for k in range(0, 4):
+                        m2[i][j] += self.m[i][k] * m.m[k][j]
+
+            return Matrix4(m2)
+        elif isinstance(m, vector4.Vector4):
+            x = m.x * self.m[0][0] + m.y * self.m[1][0] + m.z * self.m[2][0] + m.w * self.m[3][0]
+            y = m.x * self.m[0][1] + m.y * self.m[1][1] + m.z * self.m[2][1] + m.w * self.m[3][1]
+            z = m.x * self.m[0][2] + m.y * self.m[1][2] + m.z * self.m[2][2] + m.w * self.m[3][2]
+            w = m.x * self.m[0][3] + m.y * self.m[1][3] + m.z * self.m[2][3] + m.w * self.m[3][3]
+
+            return vector4.Vector4(x, y, z, w)
+        else:
+            raise InvalidMatrixOperationException("mult", type(self), type(m))
+
+    def __rmul__(self, m):   
+        if isinstance(m, Matrix4):
+            m2 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+            
+            for i in range(0, 4):
+                for j in range(0, 4):
+                    m2[i][j] = 0
+                    for k in range(0, 4):
+                        m2[i][j] += self.m[k][j] * m.m[i][k]
+
+            return Matrix4(m2)
+        elif isinstance(m, vector4.Vector4):
+            x = self.m[0][0] * m.x + self.m[0][1] * m.y + self.m[0][2] * m.z + self.m[0][3] * m.w
+            y = self.m[1][0] * m.x + self.m[1][1] * m.y + self.m[1][2] * m.z + self.m[1][3] * m.w
+            z = self.m[2][0] * m.x + self.m[2][1] * m.y + self.m[2][2] * m.z + self.m[2][3] * m.w
+            w = self.m[3][0] * m.x + self.m[3][1] * m.y + self.m[3][2] * m.z + self.m[3][3] * m.w
+
+            return vector4.Vector4(x, y, z, w)
+        else:
+            raise InvalidMatrixOperationException("mult", type(self), type(m))
+
+    def __getitem__(self, n):
+        if isinstance(n, tuple):
+            return self.m[n[0]][n[1]]
+        return self.m[n]
+
+    def __setitem__(self, n, value):
+        if isinstance(n, tuple):
+            self.m[n[0]][n[1]] = value
+        else:
+            self.m[n] = value
+
+    def premultiply_v3(self, v, w):
+        ox = v.x * self.m[0][0] + v.y * self.m[1][0] + v.z * self.m[2][0] + w * self.m[3][0]
+        oy = v.x * self.m[0][1] + v.y * self.m[1][1] + v.z * self.m[2][1] + w * self.m[3][1]
+        oz = v.x * self.m[0][2] + v.y * self.m[1][2] + v.z * self.m[2][2] + w * self.m[3][2]
+        ow = v.x * self.m[0][3] + v.y * self.m[1][3] + v.z * self.m[2][3] + w * self.m[3][3]
+
+        return vector4.Vector4(ox, oy, oz, ow)
+
+    def premultiply_v4(self, v):
+        x = v.x * self.m[0][0] + v.y * self.m[1][0] + v.z * self.m[2][0] + v.w * self.m[3][0]
+        y = v.x * self.m[0][1] + v.y * self.m[1][1] + v.z * self.m[2][1] + v.w * self.m[3][1]
+        z = v.x * self.m[0][2] + v.y * self.m[1][2] + v.z * self.m[2][2] + v.w * self.m[3][2]
+        w = v.x * self.m[0][3] + v.y * self.m[1][3] + v.z * self.m[2][3] + v.w * self.m[3][3]
+
+        return vector4.Vector4(x, y, z, w)
+
+    def posmultiply_v3(self, v, w):
+        ox = self.m[0][0] * v.x + self.m[0][1] * v.y + self.m[0][2] * v.z + self.m[0][3]
+        oy = self.m[1][0] * v.x + self.m[1][1] * v.y + self.m[1][2] * v.z + self.m[1][3]
+        oz = self.m[2][0] * v.x + self.m[2][1] * v.y + self.m[2][2] * v.z + self.m[2][3]
+        ow = self.m[3][0] * v.x + self.m[3][1] * v.y + self.m[3][2] * v.z + self.m[3][3]
+
+        return vector4.Vector4(ox, oy, oz, ow)
+
+    def posmultiply_v4(self, v):
+        x = self.m[0][0] * v.x + self.m[0][1] * v.y + self.m[0][2] * v.z + self.m[0][3] * v.w
+        y = self.m[1][0] * v.x + self.m[1][1] * v.y + self.m[1][2] * v.z + self.m[1][3] * v.w
+        z = self.m[2][0] * v.x + self.m[2][1] * v.y + self.m[2][2] * v.z + self.m[2][3] * v.w
+        w = self.m[3][0] * v.x + self.m[3][1] * v.y + self.m[3][2] * v.z + self.m[3][3] * v.w
+
+        return vector4.Vector4(x, y, z, w)
+
+    def invert(self):
+        # Use linear arrays instead of arrays of arrays
+        tmp = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        src = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        dst = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+        # Create linear arry with transposed matrix
+        for i in range(0, 4):
+            for j in range(0, 4):
+                src[i * 4 + j] = self.m[j][i]
+
+        # Calculate pairs for first 8 elements (cofactors) 
+        tmp[0]=src[10]*src[15]
+        tmp[1]=src[11]*src[14]
+        tmp[2]=src[9]*src[15]
+        tmp[3]=src[11]*src[13]
+        tmp[4]=src[9]*src[14]
+        tmp[5]=src[10]*src[13]
+        tmp[6]=src[8]*src[15]
+        tmp[7]=src[11]*src[12]
+        tmp[8]=src[8]*src[14]
+        tmp[9]=src[10]*src[12]
+        tmp[10]=src[8]*src[13]
+        tmp[11]=src[9]*src[12]
+        
+        # calculate first 8 elements (cofactors)
+        dst[0]=tmp[0]*src[5]+tmp[3]*src[6]+tmp[4]*src[7]
+        dst[0]-=tmp[1]*src[5]+tmp[2]*src[6]+tmp[5]*src[7]
+        dst[1]=tmp[1]*src[4]+tmp[6]*src[6]+tmp[9]*src[7]
+        dst[1]-=tmp[0]*src[4]+tmp[7]*src[6]+tmp[8]*src[7]
+        dst[2]=tmp[2]*src[4]+tmp[7]*src[5]+tmp[10]*src[7]
+        dst[2]-=tmp[3]*src[4]+tmp[6]*src[5]+tmp[11]*src[7]
+        dst[3]=tmp[5]*src[4]+tmp[8]*src[5]+tmp[11]*src[6]
+        dst[3]-=tmp[4]*src[4]+tmp[9]*src[5]+tmp[10]*src[6]
+        dst[4]=tmp[1]*src[1]+tmp[2]*src[2]+tmp[5]*src[3]
+        dst[4]-=tmp[0]*src[1]+tmp[3]*src[2]+tmp[4]*src[3]
+        dst[5]=tmp[0]*src[0]+tmp[7]*src[2]+tmp[8]*src[3]
+        dst[5]-=tmp[1]*src[0]+tmp[6]*src[2]+tmp[9]*src[3]
+        dst[6]=tmp[3]*src[0]+tmp[6]*src[1]+tmp[11]*src[3]
+        dst[6]-=tmp[2]*src[0]+tmp[7]*src[1]+tmp[10]*src[3]
+        dst[7]=tmp[4]*src[0]+tmp[9]*src[1]+tmp[10]*src[2]
+        dst[7]-=tmp[5]*src[0]+tmp[8]*src[1]+tmp[11]*src[2]
+
+        # calculate pairs for second 8 elements (cofactors) 
+        tmp[0]=src[2]*src[7]
+        tmp[1]=src[3]*src[6]
+        tmp[2]=src[1]*src[7]
+        tmp[3]=src[3]*src[5]
+        tmp[4]=src[1]*src[6]
+        tmp[5]=src[2]*src[5]
+        tmp[6]=src[0]*src[7]
+        tmp[7]=src[3]*src[4]
+        tmp[8]=src[0]*src[6]
+        tmp[9]=src[2]*src[4]
+        tmp[10]=src[0]*src[5]
+        tmp[11]=src[1]*src[4]
+
+        # calculate second 8 elements (cofactors) 
+        dst[8]=tmp[0]*src[13]+tmp[3]*src[14]+tmp[4]*src[15]
+        dst[8]-=tmp[1]*src[13]+tmp[2]*src[14]+tmp[5]*src[15]
+        dst[9]=tmp[1]*src[12]+tmp[6]*src[14]+tmp[9]*src[15]
+        dst[9]-=tmp[0]*src[12]+tmp[7]*src[14]+tmp[8]*src[15]
+        dst[10]=tmp[2]*src[12]+tmp[7]*src[13]+tmp[10]*src[15]
+        dst[10]-=tmp[3]*src[12]+tmp[6]*src[13]+tmp[11]*src[15]
+        dst[11]=tmp[5]*src[12]+tmp[8]*src[13]+tmp[11]*src[14]
+        dst[11]-=tmp[4]*src[12]+tmp[9]*src[13]+tmp[10]*src[14]
+        dst[12]=tmp[2]*src[10]+tmp[5]*src[11]+tmp[1]*src[9]
+        dst[12]-=tmp[4]*src[11]+tmp[0]*src[9]+tmp[3]*src[10]
+        dst[13]=tmp[8]*src[11]+tmp[0]*src[8]+tmp[7]*src[10]
+        dst[13]-=tmp[6]*src[10]+tmp[9]*src[11]+tmp[1]*src[8]
+        dst[14]=tmp[6]*src[9]+tmp[11]*src[11]+tmp[3]*src[8]
+        dst[14]-=tmp[10]*src[11]+tmp[2]*src[8]+tmp[7]*src[9]
+        dst[15]=tmp[10]*src[10]+tmp[4]*src[8]+tmp[9]*src[9]
+        dst[15]-=tmp[8]*src[9]+tmp[11]*src[10]+tmp[5]*src[8]
+        # calculate determinant 
+
+        det=src[0]*dst[0]+src[1]*dst[1]+src[2]*dst[2]+src[3]*dst[3]
+        
+        # calculate inverse
+        det=1/det
+        for i in range(0,16):
+            dst[i] *= det
+
+        for i in range(0,4):
+            for j in range(0,4):
+                self.m[i][j] = dst[i * 4 + j]
+
+    @staticmethod
+    def zeros():
+        return Matrix4(0)
+
+    @staticmethod
+    def identity():
+        m = Matrix4(0)
+        m.m[0][0] = m.m[1][1] = m.m[2][2] = m.m[3][3] = 1
+        return m
+
+# The following code is used to setup Numpy versions of the matrix operations,
+# for better performance
+if USE_NUMPY:
+    import numpy as np
+    print("Using Numpy")
+    
+    def numpy_matrix___init__(self, m = None):
+        if (m is None):
+            self.m = np.identity(4)
+        elif isinstance(m, (int, float)):
+            self.m = np.zeros((4, 4))
+            self.m.fill(m)
+        elif isinstance(m, np.ndarray):
+            self.m = m
+        else:
+            self.m = np.array(m)
+
+    def numpy_matrix_zeros():
+        return Matrix4(0)
+
+    def numpy_matrix_identity():
+        return Matrix4()
+
+    def numpy_matrix___getitem__(self, n):
+        return self.m[n]
+
+    def numpy_matrix___setitem__(self, n, value):
+        self.m[n] = value
+
+    def numpy_matrix___mul__(self, m):   
+        if isinstance(m, Matrix4):
+            return Matrix4(self.m @ m.m)
+        elif isinstance(m, vector4.Vector4):
+            v = np.array([m.x, m.y, m.z, m.w])
+            v = v @ self.m
+            return vector4.Vector4(v[0], v[1], v[2], v[3])
+        else:
+            raise InvalidMatrixOperationException("mult", type(self), type(m))
+
+    def numpy_matrix___rmul__(self, m):   
+        if isinstance(m, Matrix4):
+            return Matrix4(m.m @ self.m)
+        elif isinstance(m, vector4.Vector4):
+            v = np.array([m.x, m.y, m.z, m.w]).transpose()
+            v = self.m @ v
+            return vector4.Vector4(v[0], v[1], v[2], v[3])
+        else:
+            raise InvalidMatrixOperationException("mult", type(self), type(m))
+
+    def numpy_matrix_premultiply_v3(self, v, w):
+        v = np.array([v.x, v.y, v.z, w]) @ self.m
+
+        return vector4.Vector4(v[0], v[1], v[2], v[3])
+
+    def numpy_matrix_premultiply_v4(self, v):
+        v = np.array([v.x, v.y, v.z, v.w]) @ self.m
+
+        return vector4.Vector4(v[0], v[1], v[2], v[3])
+
+    def numpy_matrix_posmultiply_v3(self, v, w):
+        v = self.m @ np.array([v.x, v.y, v.z, w])
+
+        return vector4.Vector4(v[0], v[1], v[2], v[3])
+
+    def numpy_matrix_posmultiply_v4(self, v):
+        v = self.m @ np.array([v.x, v.y, v.z, v.w])
+
+        return vector4.Vector4(v[0], v[1], v[2], v[3])
+
+    def numpy_matrix_invert(self):
+        self.m = np.linalg.inv(self.m)
+
+    Matrix4.__init__ = numpy_matrix___init__
+    Matrix4.identity = numpy_matrix_identity
+    Matrix4.zeros = numpy_matrix_zeros
+    Matrix4.__getitem__ = numpy_matrix___getitem__
+    Matrix4.__setitem__ = numpy_matrix___setitem__
+    Matrix4.__mul__ = numpy_matrix___mul__
+    Matrix4.__rmul__ = numpy_matrix___rmul__
+    Matrix4.premultiply_v3 = numpy_matrix_premultiply_v3
+    Matrix4.premultiply_v4 = numpy_matrix_premultiply_v4
+    Matrix4.invert = numpy_matrix_invert
+
+else:
+    print("Not using Numpy")
---
+++ viewer_aplication/mesh.py
@@ -0,0 +1,395 @@
+"""Mesh class definition"""
+# Just import time if we need statistics
+import time
+import math
+import pygame
+from vector3 import Vector3
+import matrix4
+import numpy
+
+
+class Mesh:
+    """Mesh class.
+    Stores a list of polygons to be drawn
+    """
+    stat_vertex_count = 0
+    """Vertex count for statistics. This code that actually tracks the statistics
+    is normally commented out for performance reasons (see render method)"""
+    stat_transform_time = 0
+    """Time spent on vertex transforming for statistics. This code that actually tracks
+    the statistics is normally commented out for performance reasons (see render method)"""
+    stat_render_time = 0
+    """Time spent in rendering for statistics. This code that actually tracks the statistics
+    is normally commented out for performance reasons (see render method)"""
+
+    def __init__(self, name="UnknownMesh"):
+        """
+        Arguments:
+
+            name {str} -- Name of the material, defaults to 'UnknownMesh'
+        """
+        self.name = name
+        """ {str} Name of the mesh"""
+        self.polygons = []
+        """ {list[list[Vector3]]} List of lists of polygons. A polygon is a closed shape,
+        hence the need for a list of lists, if we want more complex shapes."""
+
+    def offset(self, v):
+        """
+        Offsets this mesh by a given vector. In practice, adds v to all vertex in all polygons
+
+        Arguments:
+
+            v {Vector3} -- Ammount to displace the mesh
+        """
+        new_polys = []
+        for poly in self.polygons:
+            new_poly = []
+            for p in poly:
+                new_poly.append(p + v)
+            new_polys.append(new_poly)
+
+        self.polygons = new_polys
+
+    def render(self, screen, clip_matrix, material):
+        """
+        Renders the mesh.
+
+        Arguments:
+
+            screen {pygame.surface} -- Display surface on which to render the mesh
+
+            clip_matrix {Matrix4} -- Clip matrix to use to convert the 3d local space coordinates
+            of the vertices to screen coordinates.
+
+            material {Material} -- Material to be used to render the mesh
+
+            Note that this function has the code that tracks statistics for the vertex count and
+            render times, but it is normally commented out, for performance reasons. If you want
+            to use the statistics, uncomment the code on this funtion.
+        """
+        # Convert Color to the pygame format
+        c = material.Color.tuple3()
+        w = screen.get_width() * 0.5
+        h = screen.get_height() * 0.5
+
+        # For all polygons
+        for poly in self.polygons:
+            # Create the list that will store (temporarily) the transformed vertices
+            tpoly = []
+            # Uncomment next 2 lines for statistics
+            Mesh.stat_vertex_count += len(poly)
+            t0 = time.time()
+            for v in poly:
+                # Multiply vertex it by the clip matrix - This function is slightly faster than doing
+                # vout = v * clip_matrix, since it doesn't have to check types or create additional
+                # Vector4 objects
+                vout = clip_matrix.premultiply_v3(v, 1)
+
+                # Finalize the transformation by converting the point from homogeneous NDC to
+                # screen coordinates (divide by w, scale it by the viewport resolution and
+                # offset it)
+                tpoly.append((w + vout.x / vout.w,
+                              h - vout.y / vout.w))
+
+            # Uncomment next line for statistics
+            t1 = time.time()
+
+            # Render
+            pygame.draw.polygon(screen, c, tpoly, material.line_width)
+
+            # Uncomment next 3 lines for statistics
+            t2 = time.time()
+            Mesh.stat_transform_time += (t1 - t0)
+            Mesh.stat_render_time += (t2 - t1)
+
+    @staticmethod
+    def create_mesh(path, mesh=None):
+
+        if mesh is None:
+            mesh = Mesh("UnknownMesh")
+
+        vertices = []
+        faces = []
+
+        try:
+            f = open(path)
+            for line in f:
+                if line [:2] == "v ":
+                    index1 = line.find(" ") + 1
+                    index2 = line.find(" ", index1 + 1)
+                    index3 = line.find(" ", + index2 + 1)   
+
+                    vertex = Vector3(float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]))
+                    vertex = Vector3(round(vertex.x, 2), round(vertex.y, 2),  round(vertex.z, 2))
+
+                    vertices.append(vertex)
+
+                elif line[0] == "f":
+
+                    string = line.replace("//", "/")
+
+                    i = string.find(" ") + 1
+                    face = []
+
+                    for item in range(string.count(" ")):
+                        if string.find(" ", i) == - 1:
+                            face.append(string[i:-1])
+                            print ("teste")
+                            break
+                        face.append(string[i:string.find(" ",i)])
+                        i = string.find(" ", i) + 1
+
+                    
+                    faces.append(Vector3(int(face[0][:face[0].find("/")]), int(face[1][:face[1].find("/")]), int(face[2][:face[2].find("/")]))) 
+                    
+                    if len(face) >= 3:
+                        faces.append(Vector3(int(face[1][:face[1].find("/")]), int(face[2][:face[2].find("/")]), int(face[3][:face[3].find("/")])))
+
+            f.close()
+            
+        except IOError:
+            print(".obj/json file not found.") 
+
+        for face in faces:
+            Mesh.create_tri(vertices[int(face.x -1)], vertices[int(face.y -1)], vertices[int(face.z -1)], mesh)
+
+        return mesh
+
+    @staticmethod
+    def create_pyramid(numSides=6, radius=1, height=1, mesh=None):
+
+        if mesh is None:
+            mesh = Mesh("UnknownPyramid")
+        
+        vertices = []
+
+        for j in range(numSides):
+            vertices.append(Vector3(radius * numpy.cos(j *(2 * numpy.pi / numSides)), -1, radius * numpy.sin(j*(2 * numpy.pi / numSides))))
+
+        i = 0
+
+        for vertex in vertices:
+            if i >=1:
+                Mesh.create_tri(vertices[i-1], Vector3(0, height, 0), vertex, mesh)
+                Mesh.create_tri(vertices[i-1], Vector3(0, -1, 0), vertex, mesh)
+           
+            i = i + 1
+        
+        Mesh.create_tri(vertices[numSides-1], Vector3(0, height, 0), vertices[0], mesh)
+        Mesh.create_tri(vertices[numSides-1], Vector3(0, -1, 0), vertices[0], mesh)
+
+        return mesh
+
+    @staticmethod
+    def create_cube(size, mesh=None):
+        """
+        Adds the 6 polygons necessary to form a cube with the given size. If a source mesh is
+        not given, a new mesh is created.
+        This cube will be centered on the origin (0,0,0).
+
+        Arguments:
+
+            size {3-tuple} -- (x,y,z) size of the cube
+
+            mesh {Mesh} -- Mesh to add the polygons. If not given, create a new mesh
+
+        Returns:
+            {Mesh} - Mesh where the polygons were added
+        """
+
+        # Create mesh if one was not given
+        if mesh is None:
+            mesh = Mesh("UnknownCube")
+
+        # Add the 6 quads that create a cube
+        Mesh.create_quad(Vector3(size[0] * 0.5, 0, 0),
+                         Vector3(0, -size[1] * 0.5, 0),
+                         Vector3(0, 0, size[2] * 0.5), mesh)
+        Mesh.create_quad(Vector3(-size[0] * 0.5, 0, 0),
+                         Vector3(0, size[1] * 0.5, 0),
+                         Vector3(0, 0, size[2] * 0.5), mesh)
+
+        Mesh.create_quad(Vector3(0, size[1] * 0.5, 0),
+                         Vector3(size[0] * 0.5, 0),
+                         Vector3(0, 0, size[2] * 0.5), mesh)
+        Mesh.create_quad(Vector3(0, -size[1] * 0.5, 0),
+                         Vector3(-size[0] * 0.5, 0),
+                         Vector3(0, 0, size[2] * 0.5), mesh)
+
+        Mesh.create_quad(Vector3(0, 0, size[2] * 0.5),
+                         Vector3(-size[0] * 0.5, 0),
+                         Vector3(0, size[1] * 0.5, 0), mesh)
+        Mesh.create_quad(Vector3(0, 0, -size[2] * 0.5),
+                         Vector3(size[0] * 0.5, 0),
+                         Vector3(0, size[1] * 0.5, 0), mesh)
+
+        return mesh
+
+
+    @staticmethod
+    def create_sphere(size, res_lat, res_lon, mesh=None):
+        """
+        Adds the polygons necessary to form a sphere with the given size and resolution.
+         If a source mesh is not given, a new mesh is created.
+        This sphere will be centered on the origin (0,0,0).
+
+        Arguments:
+
+            size {3-tuple} -- (x,y,z) size of the sphere
+            or
+            size {number} -- radius of the sphere
+
+            res_lat {int} -- Number of subdivisions in the latitude axis
+
+            res_lon {int} -- Number of subdivisions in the longitudinal axis
+
+            mesh {Mesh} -- Mesh to add the polygons. If not given, create a new mesh
+
+        Returns:
+            {Mesh} - Mesh where the polygons were added
+        """
+        # Create mesh if one was not given
+        if mesh is None:
+            mesh = Mesh("UnknownSphere")
+
+        # Compute half-size
+        if isinstance(size, Vector3):
+            hs = size * 0.5
+        else:
+            hs = Vector3(size[0], size[1], size[2]) * 0.5
+
+        # Sphere is going to be composed by quads in most of the surface, but triangles near the
+        # poles, so compute the bottom and top vertex
+        bottom_vertex = Vector3(0, -hs.y, 0)
+        top_vertex = Vector3(0, hs.y, 0)
+
+        lat_inc = math.pi / res_lat
+        lon_inc = math.pi * 2 / res_lon
+        # First row of triangles
+        lat = -math.pi / 2
+        lon = 0
+
+        y = hs.y * math.sin(lat + lat_inc)
+        c = math.cos(lat + lat_inc)
+        for _ in range(0, res_lon):
+            p1 = Vector3(c * math.cos(lon) * hs.x, y, c * math.sin(lon) * hs.z)
+            p2 = Vector3(c * math.cos(lon + lon_inc) * hs.x, y, c * math.sin(lon + lon_inc) * hs.z)
+
+            Mesh.create_tri(bottom_vertex, p1, p2, mesh)
+
+            lon += lon_inc
+
+        # Quads in the middle
+        for _ in range(1, res_lat - 1):
+            lat += lat_inc
+
+            y1 = hs.y * math.sin(lat)
+            y2 = hs.y * math.sin(lat + lat_inc)
+            c1 = math.cos(lat)
+            c2 = math.cos(lat + lat_inc)
+
+            lon = 0
+            for _ in range(0, res_lon):
+                p1 = Vector3(c1 * math.cos(lon) * hs.x,
+                             y1,
+                             c1 * math.sin(lon) * hs.z)
+                p2 = Vector3(c1 * math.cos(lon + lon_inc) * hs.x,
+                             y1,
+                             c1 * math.sin(lon + lon_inc) * hs.z)
+                p3 = Vector3(c2 * math.cos(lon) * hs.x,
+                             y2,
+                             c2 * math.sin(lon) * hs.z)
+                p4 = Vector3(c2 * math.cos(lon + lon_inc) * hs.x,
+                             y2,
+                             c2 * math.sin(lon + lon_inc) * hs.z)
+
+                poly = []
+                poly.append(p1)
+                poly.append(p2)
+                poly.append(p4)
+                poly.append(p3)
+
+                mesh.polygons.append(poly)
+
+                lon += lon_inc
+
+        # Last row of triangles
+        lat += lat_inc
+        y = hs.y * math.sin(lat)
+        c = math.cos(lat)
+        for _ in range(0, res_lon):
+            p1 = Vector3(c * math.cos(lon) * hs.x, y, c * math.sin(lon) * hs.z)
+            p2 = Vector3(c * math.cos(lon + lon_inc) * hs.x, y, c * math.sin(lon + lon_inc) * hs.z)
+
+            Mesh.create_tri(top_vertex, p1, p2, mesh)
+
+            lon += lon_inc
+
+        return mesh
+
+
+    @staticmethod
+    def create_quad(origin, axis0, axis1, mesh):
+        """
+        Adds the vertices necessary to create a quad (4 sided coplanar rectangle).
+        If a source mesh is not given, a new mesh is created.
+
+        Arguments:
+
+            origin {Vector3} -- Center of the quad
+
+            axis0 {Vector3} -- One of the axis of the quad. This is not normalized, since the
+            length specifies half the length of that side along that axis
+
+            axis1 {Vector3} -- One of the axis of the quad. This is not normalized, since the
+            length specifies half the length of that side along that axis
+
+            mesh {Mesh} -- Mesh to add the polygons. If not given, create a new mesh
+
+        Returns:
+            {Mesh} - Mesh where the polygons were added
+        """
+        if mesh is None:
+            mesh = Mesh("UnknownQuad")
+
+        poly = []
+        poly.append(origin + axis0 + axis1)
+        poly.append(origin + axis0 - axis1)
+        poly.append(origin - axis0 - axis1)
+        poly.append(origin - axis0 + axis1)
+
+        mesh.polygons.append(poly)
+
+        return mesh
+
+    @staticmethod
+    def create_tri(p1, p2, p3, mesh):
+        """
+        Adds the vertices necessary to create a triangle
+        If a source mesh is not given, a new mesh is created.
+
+        Arguments:
+
+            p1 {Vector3} -- First vertex of the triangle
+
+            p2 {Vector3} -- Second vertex of the triangle
+
+            p3 {Vector3} -- Third vertex of the triangle
+
+            mesh {Mesh} -- Mesh to add the polygons. If not given, create a new mesh
+
+        Returns:
+            {Mesh} - Mesh where the polygons were added
+        """
+        if mesh is None:
+            mesh = Mesh("UnknownQuad")
+
+        poly = []
+        poly.append(p1)
+        poly.append(p2)
+        poly.append(p3)
+
+        mesh.polygons.append(poly)
+
+        return mesh
